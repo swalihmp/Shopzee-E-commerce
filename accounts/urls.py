a1a3a274564408e86a5192265eba02c304d54fa7
swalihmp <swalihmp438368@gmail.com>
2023-01-27 14:32:09 +0530

    first

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.register, name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('activate/<uidb64>/<token>',views.activate,name='activate'),
+    path('forgot_password/', views.forgot_password,name='forgot_password'),
+    path('resetpassword_validate/<uidb64>/<token>',views.resetpassword_validate,name='resetpassword_validate'),
+    path('resetpassword/', views.resetpassword,name='resetpassword'),
+]
