303143752d6590319f36a1c6ef2be6155a226129
swalihmp <51339828+swalihmp@users.noreply.github.com>
2023-01-27 14:55:11 +0530

    Update settings.py

--- shopzee/settings.py
+++ shopzee/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(x_5l17iut)_=)0lqgfms%_*d(6qs*=qk7-z@%1d^r%)+gym#v'
+SECRET_KEY = '###'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
