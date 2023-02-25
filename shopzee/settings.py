327bc6789339b5052afaf5ac87aae841a60883b0
swalihmp <swalihmp438368@gmail.com>
2023-02-25 14:34:25 +0530

    ys

--- shopzee/settings.py
+++ shopzee/settings.py
@@ -152,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=#
-EMAIL_HOST_USER='#'
-EMAIL_HOST_PASSWORD='#'
+EMAIL_PORT=587
+EMAIL_HOST_USER='swalihmp8@gmail.com'
+EMAIL_HOST_PASSWORD='shofnprujseqprrh'
 EMAIL_USE_TLS=True
